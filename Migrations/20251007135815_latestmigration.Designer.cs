f73b4609dd0e05310f5c2ef07b17212459b17232
Nifemi Odukoya <83613166+vertigo99x@users.noreply.github.com>
2025-10-07 14:58:49 +0100

    Initial commit for recentUpdate branch

--- Migrations/20251007135815_latestmigration.Designer.cs
+++ Migrations/20251007135815_latestmigration.Designer.cs
@@ -4,6 +4,7 @@ using CW_RETAIL.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CW_RETAIL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251007135815_latestmigration")]
+    partial class latestmigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
